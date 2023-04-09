5ad577e3bb37a3455e157c4d6b3618c54a70acfd
Ceslaviee <gabin.simonard@hotmail.com>
2023-04-09 14:53:08 +0200

    Oublier de push

--- Main.js
+++ Main.js
@@ -4,14 +4,11 @@ class Main extends Phaser.Scene {
     }
     preload() {
         this.load.image("bouton", "assets/Bouton.png");
-        //this.load.image("Logo", "assets/Logo.png");
-        //this.load.image("TitleScreen", "assets/TitleScreen.png")
+        this.load.image("TitleScreen", "assets/Titre Menu.png")
     }
     create() {
-        this.add.image(400, 225, "TitleScreen");
-        this.add.image(380, 120, "Logo").setScale(0.2);
-        this.gameButton = this.add.image(325
-            , 325, "bouton").setInteractive().setScale(0.1);
+        this.add.image(400, 225, "TitleScreen").setScale(0.25);
+        this.gameButton = this.add.image(385,325,"bouton").setInteractive().setScale(0.1);
         this.gameButton.on("pointerdown", this.launchGame, this);
     }
     update() {}
